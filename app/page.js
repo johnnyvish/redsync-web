4fc1bd41472dcdd9af96c20da92c5c8dadc85e0a
Johnny  Vishnevskiy <johnnyvishnevskiy@Johnnys-Air.fios-router.home>
2024-02-22 22:18:39 -0500

    animation test

--- app/page.js
+++ app/page.js
@@ -1,113 +1,217 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+  useEffect(() => {
+    ScrollTrigger.refresh();
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".purple-container",
+        start: "top top",
+        end: () =>
+          `+=${document.querySelector(".purple-container").offsetHeight * 8}`,
+        scrub: 1,
+        pin: true,
+        anticipatePin: 1,
+      },
+    });
+
+    tl.to(".main-container", { backgroundColor: "#ADD8E6", duration: 5 }, 0)
+      .to(".bg-green-400", { scale: 2.5, duration: 5 })
+      .to(".bg-green-400", {
+        right: "28%",
+        top: "70%",
+        scale: 1,
+        duration: 3,
+      })
+      .to(".bg-red-400", { scale: 2.5, duration: 5 })
+      .to(".bg-red-400", {
+        left: "28%",
+        top: "70%",
+        scale: 1,
+        duration: 3,
+      })
+      .to(".bg-blue-400", { scale: 2.5, duration: 5 })
+      .to(".bg-blue-400", {
+        right: "28%",
+        top: "28%",
+        scale: 1,
+        duration: 3,
+      })
+      .to(".bg-yellow-400", { scale: 2.5, duration: 5 })
+      .to(".bg-yellow-400", {
+        left: "28%",
+        top: "28%",
+        scale: 1,
+        duration: 3,
+      });
+
+    return () => {
+      ScrollTrigger.getAll().forEach((instance) => instance.kill());
+    };
+  }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="main-container flex flex-col w-full bg-[#fcf0f4]">
+      <div className="fixed top-8 left-8 flex text-3xl font-bold z-[1000] bg-white rounded-[2rem] pt-2 pb-2 pl-4 pr-4">
+        <h1 className="text-red-800">Red</h1>
+        <h1 className="text-gray-800">Sync</h1>
+      </div>
+      <nav className="fixed top-8 z-[1000] w-full px-4 md:px-0">
+        <div className="max-w-[95%] md:max-w-none mx-auto md:mx-0 md:absolute md:right-10">
+          <ul className="flex justify-center md:justify-end space-x-4 font-semibold bg-white/90 rounded-[32px] pl-8 pr-8 pt-4 pb-4 text-md sm:text-xl text-gray-800">
+            <li>
+              <a href="#" className="hover:text-red-800">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-red-800">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-red-800">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-red-800">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="min-h-screen flex flex-col justify-center items-center text-4xl md:text-8xl font-bold text-gray-800">
+        <div className="flex justify-center items-center md:w-[800px]">
+          <div className="sync flex flex-col space-y-4 justify-center items-center opacity-1">
+            <h2 className="text-black text-3xl md:text-6xl self-center text-center">
+              Don't treat health symptoms.
+            </h2>
+            <h2 className="text-black text-3xl md:text-6xl self-center text-center">
+              Prevent{" "}
+              <span className="disease text-red-800">heart disease</span>.
+            </h2>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      {/* Replace <img> with <Image> from next/image */}
+      <div className="icon absolute left-[16%] md:left-[10%] top-[60%] -translate-x-1/2 -translate-y-1/2 h-16 w-16 md:h-28 md:w-28 -rotate-12">
+        <Image src="/heart.svg" alt="Heart" layout="fill" objectFit="contain" />
+      </div>
+      <div className="icon absolute left-[24%] md:left-[12%] top-[24%] -translate-x-1/2 -translate-y-1/2 h-16 w-16 md:h-28 md:w-28 -rotate-12">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/blood-pressure.svg"
+          alt="Blood Pressure"
+          layout="fill"
+          objectFit="contain"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="icon absolute left-[12%] md:left-[20%] top-[30%] -translate-x-1/2 -translate-y-1/2 h-16 w-16 md:h-28 md:w-28 -rotate-12">
+        <Image src="/water.svg" alt="Water" layout="fill" objectFit="contain" />
       </div>
+      <div className="icon absolute left-[28%] md:left-[24%] top-[80%] -translate-x-1/2 -translate-y-1/2 h-16 w-16 md:h-28 md:w-28 -rotate-12">
+        <Image src="/scale.svg" alt="Scale" layout="fill" objectFit="contain" />
+      </div>
+      <div className="icon absolute right-[12%] md:right-[10%] top-[60%] translate-x-1/2 -translate-y-1/2 h-16 w-16 md:h-28 md:w-28 rotate-12">
+        <Image src="/food.svg" alt="Food" layout="fill" objectFit="contain" />
+      </div>
+      <div className="icon absolute right-[16%] md:right-[20%] top-[30%] translate-x-1/2 -translate-y-1/2 h-16 w-16 md:h-28 md:w-28 rotate-12">
+        <Image src="/tape.svg" alt="Tape" layout="fill" objectFit="contain" />
+      </div>
+      <div className="icon absolute right-[14%] md:right-[24%] top-[80%] translate-x-1/2 -translate-y-1/2 h-16 w-16 md:h-28 md:w-28 rotate-12">
+        <Image src="/blood.svg" alt="Blood" layout="fill" objectFit="contain" />
+      </div>
+      <div className="icon absolute right-[10%] md:right-[5%] top-[70%] md:top-[80%] translate-x-1/2 -translate-y-1/2 h-16 w-16 md:h-28 md:w-28 rotate-12">
+        <Image src="/sleep.svg" alt="Sleep" layout="fill" objectFit="contain" />
+      </div>
+
+      <div className="info-box absolute flex justify-center items-center h-[340px] w-[92%] md:w-[480px] bg-blue-200 rounded-[32px] left-1/2 top-[928px] md:top-[892px] -translate-x-1/2 -translate-y-1/2 shadow-2xl">
+        <div className="flex flex-col items-center space-y-8 text-center text-md md:text-xl p-4 text-black">
+          <h2 className="text-3xl md:text-4xl font-bold text-center">
+            How does it work?
+          </h2>
+          <h3 className="info-point-1">
+            1. We send you medical-grade sensors. 📦🔬
+          </h3>
+          <h3 className="info-point-2">
+            2. You take daily and weekly measurements of your body. 📆📏
+          </h3>
+          <h3 className="info-point-3">
+            3. Red provide you insights and keep you accountable for action.📈🔴
+          </h3>
+        </div>
+      </div>
+      <div className="section-2 min-h-screen relative flex justify-center items-center w-[98%] self-center mt-40 md:mt-48 rounded-[32px]"></div>
+      <div className="purple-container min-h-screen relative flex justify-center items-center w-[98%] self-center mt-40 md:mt-48 rounded-[32px]">
+        <div className="z-[995] absolute flex flex-col space-y-4 justify-center items-center left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 bg-yellow-400 h-[100px] w-[100px] md:h-40 md:w-40 lg:h-48 lg:w-48 rounded-[2rem] shadow-2xl floating">
+          <img
+            src="/blood-pressure-monitor.png"
+            className="self-center rounded-[2rem] p-4"
+          />
+        </div>
+        <div className="z-[996] absolute flex flex-col space-y-4 justify-center items-center left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 bg-blue-400 h-[100px] w-[100px] md:h-40 md:w-40 lg:h-48 lg:w-48 rounded-[2rem] shadow-2xl floating">
+          <img
+            src="/glucose-sensor.png"
+            className="self-center rounded-[2rem] p-4"
+          />
+        </div>
+        <div className="z-[997] absolute flex flex-col space-y-4 justify-center items-center left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 bg-red-400 h-[100px] w-[100px] md:h-40 md:w-40 lg:h-48 lg:w-48 rounded-[2rem] shadow-2xl floating">
+          <img
+            src="/scale-sensor.png"
+            className="self-center rounded-[2rem] p-4"
+          />
+        </div>
+        <div className="z-[998] absolute flex flex-col space-y-4 justify-center items-center left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 bg-green-400 h-[100px] w-[100px] md:h-40 md:w-40 lg:h-48 lg:w-48 rounded-[2rem] shadow-2xl floating">
+          <img
+            src="/smart-ring.png"
+            className="self-center rounded-[2rem] p-4"
+          />
+        </div>
+        <div className="absolute flex flex-col space-y-8 justify-center items-center left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2  rounded-[2rem]">
+          <h1 className="font-bold text-7xl text-black">$30/mo</h1>
+          <button className="flex justify-center items-center h-[42px] w-[108px] md:h-[64px] md:w-[160px] bg-red-800 rounded-[32px] shadow-2xl mt-4 md:mt-12">
+            <p className="text-[1rem] md:text-[1.3rem] text-gray-800 font-semibold text-white">
+              Subscribe
+            </p>
+          </button>
+        </div>
+      </div>
+      <div className="main-container flex justify-center items-center w-[100%] self-center mt-28 h-[800vh] bg-[#f3f3e9] rounded-[32px]"></div>
+      <footer className="w-full bg-gray-800 py-4 mt-8">
+        <div className="max-w-screen-lg mx-auto text-center">
+          <p className="text-sm text-gray-300">
+            © {new Date().getFullYear()} Red Sync. All rights reserved.
+          </p>
+          <div className="flex justify-center space-x-4 mt-2">
+            <a
+              href="#"
+              className="text-gray-400 hover:text-white transition-colors duration-300"
+            >
+              Privacy Policy
+            </a>
+            <span className="text-gray-500">|</span>
+            <a
+              href="#"
+              className="text-gray-400 hover:text-white transition-colors duration-300"
+            >
+              Terms of Use
+            </a>
+          </div>
+          <p className="text-xs text-gray-500 mt-2">
+            Designed by Red Sync Team
+          </p>
+        </div>
+      </footer>
     </main>
   );
 }
