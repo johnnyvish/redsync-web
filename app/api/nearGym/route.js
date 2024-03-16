a5181dbd23c98a31fa09327d620ab5024f4f0477
Johnny  Vishnevskiy <johnnyvishnevskiy@Johnnys-Air.fios-router.home>
2024-03-16 15:09:41 -0400

    nearGym returns name and address

--- app/api/nearGym/route.js
+++ app/api/nearGym/route.js
@@ -23,27 +23,33 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
   // Distance in kilometers
   const distance = R * c;
 
-  return distance;
+  return distance * 1000;
 }
 
 async function findNearestGym(latitude, longitude) {
   const apiKey = process.env.GOOGLE_MAPS_API_KEY; // Accessing the API key
-  const radius = 5000; // Search within a 5-kilometer radius
+  const radius = 1000; // Search within a 5-kilometer radius
   const type = "gym";
   const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&type=${type}&key=${apiKey}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    // Assuming you have a way to calculate distance, or you can sort by proximity based on Google's ranking
-    // This part is simplified; you'll need to implement logic based on your specific needs
-    const nearestGym = data.results[0]; // Example: Taking the first result as the nearest for simplicity
-    return calculateDistance(
+    const nearestGym = data.results[0]; // Taking the first result as the nearest
+
+    const distance = calculateDistance(
       latitude,
       longitude,
       nearestGym.geometry.location.lat,
       nearestGym.geometry.location.lng
     );
+
+    // Including gym name and address
+    return {
+      distance, // Distance in meters
+      name: nearestGym.name, // Gym name
+      address: nearestGym.vicinity, // Gym address
+    };
   } catch (error) {
     console.error("Failed to find nearest gym:", error);
     throw new Error("Failed to find nearest gym");
@@ -56,9 +62,9 @@ export async function POST(request) {
     const latitude = req.latitude;
     const longitude = req.longitude;
 
-    const distance = await findNearestGym(latitude, longitude);
+    const result = await findNearestGym(latitude, longitude);
 
-    return new NextResponse(JSON.stringify({ result: distance }), {
+    return new NextResponse(JSON.stringify(result), {
       status: 200,
     });
   } catch (error) {
